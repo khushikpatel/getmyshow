734d2320abffa00a4db489809928902b3d32c322
khushi patel <khushikpatel1811@gmail.com>
2024-10-17 00:27:28 +0530

    first commit

---
+++ Movie_6/JS_movie.js
@@ -0,0 +1,14 @@
+
+function trailer(){
+    var a= document.getElementById("imp");
+    var b = document.getElementById("frame");
+    var c=document.getElementById("bt");
+    var d=document.getElementById("sec");
+    a.style.opacity="0";
+    b.style.visibility="visible";
+    c.style.visibility="visible";
+    d.style.visibility="visible";
+    
+}
+
+   
